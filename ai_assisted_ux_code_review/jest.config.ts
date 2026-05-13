a35f2d4468e12db9cf47329559f6175fc34d27bb
Yue Lin <yue.lin@mongodb.com>
2026-05-13 12:41:00 -0400

    fix: correct jest config and add app directory

--- ai_assisted_ux_code_review/jest.config.ts
+++ ai_assisted_ux_code_review/jest.config.ts
@@ -4,9 +4,8 @@ import nextJest from 'next/jest';
 const createJestConfig = nextJest({ dir: './' });
 
 const config: Config = {
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathPattern: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
 };
 
 export default createJestConfig(config);
