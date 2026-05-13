f9e3b43ec41fd681714381d49a1532192aa18242
Yue Lin <yue.lin@mongodb.com>
2026-05-13 15:49:04 -0400

    feat: add LeafyGreenProvider to root layout

--- ai_assisted_ux_code_review/app/layout.tsx
+++ ai_assisted_ux_code_review/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <LeafyGreenProvider>{children}</LeafyGreenProvider>
+      </body>
     </html>
   );
 }
